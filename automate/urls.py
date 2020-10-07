cabc436ad30e29dca0a93f5a40886e367c5342e5
Amithnc <53530365+Amithnc@users.noreply.github.com>
2020-10-08 00:09:16 +0530

    Added new view to the employee creation part
    :arrow_right:  added a new view to verify the employee details stored in the excel file .

--- automate/urls.py
+++ automate/urls.py
@@ -1,7 +1,7 @@
 from django.urls import path
 from . import views
 urlpatterns = [
-    path('create-employee',views.createuser),
+    path('create-employee/<int:id>',views.createuser),
     path('update-details/<int:id>/',views.updatedetails,name='update-details'),
-    path('verify-employee',views.verifyemployee)
+    path('verify-employee/<int:id>/',views.verifyemployee)
 ]
--- automate/views.py
+++ automate/views.py
@@ -14,39 +14,56 @@ LOCASE_CHARACTERS = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h','i', 'j', 'k', 'm', 
 UPCASE_CHARACTERS = ['A', 'B', 'C', 'D', 'E', 'F', 'G', 'H','I', 'J', 'K', 'M', 'N', 'O', 'p', 'Q','R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z']        
 
 
-def homepage(request):
-    # data=read_excel("D:\\web\\employee.xlsx")
-    # # mobile=[];name=[];email=[]
-    context={}
-    # User = get_user_model()
-    # for i in range(len(data)):
-    #     username=data["Name"][i]
-    #     email=data["email"][i]
-    #     password=""
-    #     for _ in range(2):
-    #         password+=random.choice(DIGITS)+random.choice(LOCASE_CHARACTERS)+random.choice(UPCASE_CHARACTERS)
-          
-    #     # mobile.append(data["mobile"][i]);name.append(data["Name"][i]);email.append(data["email"][i])
-    #     if User.objects.filter(username=username).exists():
-    #         continue
-    #     else:
-    #         User.objects.get_or_create(username=username, is_staff=False) 
-    #         u = User.objects.get(username=username)   #
-    #         u.set_password(password)
-    #         u.email=email
-    #         # server = smtplib.SMTP("smtp.gmail.com", 587)
-    #         # server.starttls()
-    #         # server.login(SenderAddress, pswd)
-    #         # msg = "hello" + username +"below are the credentials that you need to check your payroll details"+"\n"+"your username is:\t"+username+"\nyour password is:\t"+password+"\n link to the website:\t http://127.0.0.1:8000/"+"\n\n"+"NOTE:please change your  password once u login\n"+"\n\n"+"-technical team @NCA"
-    #         # subject = "Payroll login credentials"
-    #         # body = "Subject: {}\n\n{}".format(subject,msg)
-    #         # server.sendmail(SenderAddress, email, body)
-    #         print("created and mail sent to",username)
-    #         u.save()
-    #     # server.quit()   
-    # # context['mainlist']=zip(name,mobile,email)
-    return render(request,'home.html',context)
+def createuser(request,id):
+    instance=get_object_or_404(employee,id=id) 
+    filename=instance.employee_file
+    data=read_excel(filename)
+    User = get_user_model()
+    for i in range(len(data)):
+        username=data["Name"][i]   
+        if User.objects.filter(username=username).exists():
+            continue
+        else:
+            email=data["email"][i]
+            password=""
+            for _ in range(2):
+                password+=random.choice(DIGITS)+random.choice(LOCASE_CHARACTERS)+random.choice(UPCASE_CHARACTERS)
+            User.objects.get_or_create(username=username, is_staff=False) 
+            u = User.objects.get(username=username)   #
+            u.set_password(password)
+            u.email=email
+            # server = smtplib.SMTP("smtp.gmail.com", 587)
+            # server.starttls()
+            # server.login(SenderAddress, pswd)
+            # msg = "hello" + username +"below are the credentials that you need to check your payroll details"+"\n"+"your username is:\t"+username+"\nyour password is:\t"+password+"\n link to the website:\t http://127.0.0.1:8000/"+"\n\n"+"NOTE:please change your  password once u login\n"+"\n\n"+"-technical team @NCA"
+            # subject = "Payroll login credentials"
+            # body = "Subject: {}\n\n{}".format(subject,msg)
+            # server.sendmail(SenderAddress, email, body)
+            print("created and mail sent to",username)
+            u.save()
+        # server.quit()   
+    url="/verify-employee/"+str(id)
+    return redirect(url) 
 
+
+def verifyemployee(request,id):
+    instance=get_object_or_404(employee,id=id) 
+    filename=instance.employee_file
+    data=read_excel(filename)
+    name=[];email=[];status=[];context={};flag=0
+    User = get_user_model()
+    for i in range(len(data)):
+        if User.objects.filter(username=data["Name"][i]).exists():
+            status.append(0)
+        else:
+            status.append(1)    
+        name.append(data["Name"][i]);email.append(data["email"][i])
+    if status.count(1)==0:
+        flag=1   
+    context['mainlist']=zip(name,email,status)
+    context['flag']=flag
+    context['id']=instance.id
+    return render(request,'verifyemployee.html',context)
 
 def updatedetails(request,id):
     instance = get_object_or_404(employee,id=id)  
@@ -54,7 +71,8 @@ def updatedetails(request,id):
         form = registerForm(request.POST  or None,files=request.FILES,instance = instance)
         if form.is_valid():
             form.save()
-            return redirect("/")
+            url="/verify-employee/"+str(id)
+            return redirect(url)
     else:
         form = registerFormnovalidate(request.POST  or None,files=request.FILES,instance = instance)        
     context={}
